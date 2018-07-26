1ec3432077a5e477028c499a87d65d78d5da78fa
Eliton Santos <eliton.santos@senior.com.br>
2018-07-26 15:36:53 -0300

    Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../../node_modules/primeng/components/common/menuitem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  visibleSidebar =true;
+
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'File',
+            icon: 'fa fa-fw fa-file-o',
+            items: [{
+                    label: 'New', 
+                    icon: 'fa fa-fw fa-plus',
+                    items: [
+                        {label: 'Project'},
+                        {label: 'Other'},
+                    ]
+                },
+                {label: 'Open'},
+                {label: 'Quit'}
+            ]
+        },
+        {
+            label: 'Edit',
+            icon: 'fa fa-fw fa-edit',
+            items: [
+                {label: 'Undo', icon: 'fa fa-fw fa-mail-forward'},
+                {label: 'Redo', icon: 'fa fa-fw fa-mail-reply'}
+            ]
+        },
+        {
+            label: 'Help',
+            icon: 'fa fa-fw fa-question',
+            items: [
+                {
+                    label: 'Contents'
+                },
+                {
+                    label: 'Search', 
+                    icon: 'fa fa-fw fa-search', 
+                    items: [
+                        {
+                            label: 'Text', 
+                            items: [
+                                {
+                                    label: 'Workspace'
+                                }
+                            ]
+                        },
+                        {
+                            label: 'File'
+                        }
+                ]}
+            ]
+        },
+        {
+            label: 'Actions',
+            icon: 'fa fa-fw fa-gear',
+            items: [
+                {
+                    label: 'Edit',
+                    icon: 'fa fa-fw fa-refresh',
+                    items: [
+                        {label: 'Save', icon: 'fa fa-fw fa-save'},
+                        {label: 'Update', icon: 'fa fa-fw fa-save'},
+                    ]
+                },
+                {
+                    label: 'Other',
+                    icon: 'fa fa-fw fa-phone',
+                    items: [
+                        {label: 'Delete', icon: 'fa fa-fw fa-minus'}
+                    ]
+                }
+            ]
+        }
+    ];
+}
+
+  showHide(){
+    this.visibleSidebar = !this.visibleSidebar;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MenuModule} from 'primeng/menu';
 import { NgModule } from '@angular/core';
+import {SidebarModule} from 'primeng/sidebar';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {ButtonModule} from 'primeng/button';
+import {PanelMenuModule} from 'primeng/panelmenu';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+    SlideMenuModule,
+    MenuModule,
+    ButtonModule,
+    PanelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
